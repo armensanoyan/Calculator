88824fd1c87fbe0c64f746e2d29a89b74d4c18b1
Armen Sanoyan <armensanoyan@gmail.com>
2020-12-16 00:43:52 +0400

    impliment calculaitons"first demo"

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 # !/usr/bin/python3  
 import tkinter as tk
 from functools import partial
+import operator
 
+operations_dict = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
 entry_number = ''
-operations = ['0','+','-','*','/','=']
-list_of_buttons = list(range(10)) + operations
+operations = ['+','-','*','/','=']
+list_of_buttons = list(range(10)) + ['0'] + operations
+current_value =''
+current_operation =''
+first_value = ''
 
 def main():
 
@@ -13,9 +23,11 @@ def main():
     window.title("Calculator")
     input_area = tk.Frame(window)
     input_area.pack()
+
     entry = tk.Entry(input_area, fg="yellow", bg="Gray")
+    entry.bind('<KeyRelease>', get_keyboard_input)
     entry.pack()
-    entry.bind('<KeyRelease>', hello)
+
     main_frame = tk.Frame(input_area, width=150, height=50)
     main_frame.pack()
 
@@ -24,27 +36,44 @@ def main():
         for j in range(3):
             frame = tk.Frame(main_frame, relief=tk.RAISED, borderwidth=1,)
             frame.grid(row=i, column=j)
-            label = tk.Button(frame, text= list_of_buttons[number], relief=tk.RAISED, fg='Gray', bg='DimGray', width=10, height=3,command = partial(pushed, number, entry))
+            label = tk.Button(frame, text= list_of_buttons[number], relief=tk.RAISED, fg='Gray', bg='DimGray', width=10, height=3)
+            label.bind('<Button-1>',get_pushed_button)
             number += 1
 
             label.pack(fill='both')
 
     window.mainloop()
 
-def hello(event):
-    entry_number = event.widget.get()
-    current_value = event.keysym
-    try:
-        int(current_value)
-    except ValueError:
-        event.widget.delete(len(entry_number)-1,len(entry_number))
-        print('not number') 
-    print(event.widget.get())
 
-def pushed(arg, entry):
-    def get_button(arg):
-        entry.insert(0,entry_number + str(list_of_buttons[arg]))
-        print('pushed: ', list_of_buttons[arg])
-    return get_button(arg)
+def get_pushed_button(event):
+    current_value = event.widget['text']
+    entry = event.widget.master.master.master.children['!entry']
+
+    if type(current_value) == int or current_value.isdigit():
+        entry.insert(len(entry.get()), str(list_of_buttons[current_value]))
+    elif current_value in operations:
+        global first_value
+        global current_operation
+        if first_value != '':
+            current_value = operations_dict[current_operation](int(first_value), int(entry.get()))
+            first_value = current_value
+            entry.delete(0,len(entry.get()))
+            entry.insert(0, current_value)
+        else:
+            current_operation = current_value
+            first_value = entry.get()
+            entry.delete(0,len(entry.get()))
+    print(current_value)
+
+
+def get_keyboard_input(event):
+    entry = event.widget
+    entry_number = entry.get()
+    current_value = event.keysym
+
+    if not current_value.isdigit() and not current_value in operations:
+        entry.delete(len(entry_number)-1,len(entry_number))
+    print(entry.get())
+
 
 main()
