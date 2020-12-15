fa3671ececb0f63a6d8c044aa5e97100fb405e6b
Armen Sanoyan <armensanoyan@gmail.com>
2020-12-15 14:37:17 +0400

    cleaned code

--- app.py
+++ app.py
@@ -2,48 +2,49 @@
 import tkinter as tk
 from functools import partial
 
+entry_number = ''
+operations = ['0','+','-','*','/','=']
+list_of_buttons = list(range(10)) + operations
 
-window = tk.Tk()
-window.geometry("300x350")  
-window.title("Calculator")
-input_area = tk.Frame(window)
-input_area.pack()
+def main():
+
+    window = tk.Tk()
+    window.geometry("300x350")  
+    window.title("Calculator")
+    input_area = tk.Frame(window)
+    input_area.pack()
+    entry = tk.Entry(input_area, fg="yellow", bg="Gray")
+    entry.pack()
+    entry.bind('<KeyRelease>', hello)
+    main_frame = tk.Frame(input_area, width=150, height=50)
+    main_frame.pack()
+
+    number = 1
+    for i in range(5):
+        for j in range(3):
+            frame = tk.Frame(main_frame, relief=tk.RAISED, borderwidth=1,)
+            frame.grid(row=i, column=j)
+            label = tk.Button(frame, text= list_of_buttons[number], relief=tk.RAISED, fg='Gray', bg='DimGray', width=10, height=3,command = partial(pushed, number, entry))
+            number += 1
+
+            label.pack(fill='both')
+
+    window.mainloop()
 
 def hello(event):
-    value = entry.get()
+    entry_number = event.widget.get()
     current_value = event.keysym
-    print(current_value)
     try:
         int(current_value)
     except ValueError:
-        entry.delete(len(value)-1,len(value))
+        event.widget.delete(len(entry_number)-1,len(entry_number))
         print('not number') 
-    print(entry.get())
+    print(event.widget.get())
 
-entry = tk.Entry(input_area, fg="yellow", bg="Gray")
-entry.pack()
-entry.bind('<KeyRelease>', hello)
-input_text = entry.get()
-print('input_text:', input_text)
-
-main_frame = tk.Frame(input_area, width=150, height=50)
-main_frame.pack()
-
-arr = list(range(10)) + ['0','+','-','*','/','%']
-
-def pushed(arg):
+def pushed(arg, entry):
     def get_button(arg):
-        print('pushed: ', arr[arg])
+        entry.insert(0,entry_number + str(list_of_buttons[arg]))
+        print('pushed: ', list_of_buttons[arg])
     return get_button(arg)
 
-number = 1
-for i in range(5):
-    for j in range(3):
-        frame = tk.Frame(main_frame, relief=tk.RAISED, borderwidth=1,)
-        frame.grid(row=i, column=j)
-        label = tk.Button(frame, text= arr[number], relief=tk.RAISED, fg='Gray', bg='DimGray', width=10, height=3,command = partial(pushed, number))
-        number += 1
-
-        label.pack(fill='both')
-
-window.mainloop()
+main()
